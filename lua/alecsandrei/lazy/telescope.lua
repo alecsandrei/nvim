7083b7d840692c7ad9d7e0633976723a8c781d34
Alex Andrei Cuvuliuc <cuvuliucalexandrei@gmail.com>
2025-05-16 11:53:13 +0300

    search symbols in workspace and document

--- lua/alecsandrei/lazy/telescope.lua
+++ lua/alecsandrei/lazy/telescope.lua
@@ -7,6 +7,13 @@ return {
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 		vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Telescope find git files" })
+		vim.keymap.set("n", "<leader>ds", builtin.lsp_document_symbols, { desc = "Find symbols in the current buffer" })
+		vim.keymap.set(
+			"n",
+			"<leader>ws",
+			builtin.lsp_workspace_symbols,
+			{ desc = "Find symbols in the current workspace" }
+		)
 		vim.keymap.set("n", "<leader>ps", function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
 		end)
