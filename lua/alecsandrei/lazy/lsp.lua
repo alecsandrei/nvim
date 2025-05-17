4e5440a6888408848f89947ad6d5e58c2b8ee80c
Alex Andrei Cuvuliuc <cuvuliucalexandrei@gmail.com>
2025-05-17 08:01:03 +0300

    map for vim.lsp.buf.hover

--- lua/alecsandrei/lazy/lsp.lua
+++ lua/alecsandrei/lazy/lsp.lua
@@ -30,6 +30,9 @@ return {
 						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 					end
 
+					-- Display information about the variable under your cursor
+					map("gh", vim.lsp.buf.hover, "[H]over")
+
 					-- Rename the variable under your cursor.
 					--  Most Language Servers support renaming across files, etc.
 					map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
