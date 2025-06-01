f9ea5b372b42508580d7bb8ca4c99dde6d7ce9c9
Alex Andrei Cuvuliuc <cuvuliucalexandrei@gmail.com>
2025-06-01 13:31:51 +0300

    add fzf extension to telescope

--- lua/alecsandrei/lazy/telescope.lua
+++ lua/alecsandrei/lazy/telescope.lua
@@ -1,8 +1,16 @@
 -- Telescope and its dependency
 return {
 	"nvim-telescope/telescope.nvim",
-	version = "0.1.8",
 	dependencies = { "nvim-lua/plenary.nvim" },
+	extensions = {
+		fzf = {
+			fuzzy = true, -- false will only do exact matching
+			override_generic_sorter = true, -- override the generic sorter
+			override_file_sorter = true, -- override the file sorter
+			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+			-- the default case_mode is "smart_case"
+		},
+	},
 	config = function()
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
@@ -10,6 +18,7 @@ return {
 		vim.keymap.set("n", "<leader>ps", function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
 		end)
+		require("telescope").load_extension("fzf")
 		-- Some more telescope maps defined in lsp.lua
 	end,
 }
