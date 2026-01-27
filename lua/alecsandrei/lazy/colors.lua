1abc65766adf7556803e63d3721fed954fc6854c
Alex-Andrei Cuvuliuc <cuvuliucalexandrei@gmail.com>
2026-01-27 08:39:01 +0100

    transparent=True with dark terminal background

--- lua/alecsandrei/lazy/colors.lua
+++ lua/alecsandrei/lazy/colors.lua
@@ -3,7 +3,31 @@ return {
 	"ellisonleao/gruvbox.nvim",
 	name = "gruvbox",
 	config = function()
-		vim.cmd.colorscheme("gruvbox")
+		require("gruvbox").setup({
+			terminal_colors = true,
+			undercurl = true,
+			underline = true,
+			bold = true,
+			italic = {
+				strings = true,
+				emphasis = true,
+				comments = true,
+				operators = false,
+				folds = true,
+			},
+			strikethrough = true,
+			invert_selection = false,
+			invert_signs = false,
+			invert_tabline = false,
+			inverse = true,
+			contrast = "", -- "hard", "soft", or ""
+			palette_overrides = {},
+			overrides = {},
+			dim_inactive = false,
+			transparent_mode = true,
+		})
+
 		vim.o.background = "dark"
+		vim.cmd("colorscheme gruvbox")
 	end,
 }
