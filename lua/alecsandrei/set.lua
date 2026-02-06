0ba41589acb90b42cd00cca8e1b5572a2d6fb58b
Alex-Andrei Cuvuliuc <cuvuliucalexandrei@gmail.com>
2026-02-06 17:02:34 +0100

    split bindings

--- lua/alecsandrei/set.lua
+++ lua/alecsandrei/set.lua
@@ -37,6 +37,9 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+vim.keymap.set("n", '<leader>"', ":vsplit<CR>", { desc = "Vertical split" })
+vim.keymap.set("n", "<leader>%", ":split<CR>", { desc = "Horizontal split" })
+
 -- AUTOCOMMANDS --
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
