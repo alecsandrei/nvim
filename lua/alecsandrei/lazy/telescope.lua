4c87356455081abd78001c53f610692539857da2
Alex-Andrei Cuvuliuc <cuvuliucalexandrei@gmail.com>
2026-02-23 10:27:41 +0100

    live grep

--- lua/alecsandrei/lazy/telescope.lua
+++ lua/alecsandrei/lazy/telescope.lua
@@ -15,6 +15,7 @@ return {
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 		vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Telescope find git files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 		vim.keymap.set("n", "<leader>ps", function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
 		end)
