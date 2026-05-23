0817045b95a41642636733fe2e6bc464a08765a8
Alex-Andrei Cuvuliuc <cuvuliucalexandrei@gmail.com>
2026-05-23 10:01:20 +0200

    make it work for mac -> should work for linux too

--- lua/alecsandrei/lazy/vimtex.lua
+++ lua/alecsandrei/lazy/vimtex.lua
@@ -2,12 +2,18 @@ return {
 	"lervag/vimtex",
 	lazy = false,
 	init = function()
-		vim.g.vimtex_view_method = "zathura"
+		vim.g.vimtex_view_method = "zathura_simple"
+		vim.g.vimtex_view_zathura_use_synctex = 0
+		vim.g.vimtex_view_automatic = 1
 
 		vim.g.vimtex_compiler_latexmk = {
 			out_dir = "build",
 			aux_dir = "build",
+			callback = 1,
+			continuous = 1,
 		}
+
+		vim.g.vimtex_quickfix_mode = 0
 
 		vim.api.nvim_create_autocmd("User", {
 			pattern = "VimtexEventInitPost",
@@ -15,7 +21,5 @@ return {
 				vim.cmd("VimtexCompile")
 			end,
 		})
-
-		vim.g.vimtex_quickfix_mode = 0
 	end,
 }
